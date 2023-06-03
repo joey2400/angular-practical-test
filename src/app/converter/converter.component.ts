85204f30c9c4127d5530258349c870258c73d76d
Joey Anthony Tuminez <joey.damedigital@gmail.com>
2023-06-03 23:51:08 +0800

    create: converter

---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent {
+  typedValue: string = '';
+  convertedValue: string = '';
+
+  onClick(value: string) {
+    this.typedValue += value;
+    console.log('You typed:', value);
+    this.convertValue();
+  }
+  removeValue() {
+    this.typedValue = this.typedValue.slice(0, -1);
+    this.convertValue();
+  }
+
+  convertValue() {
+    const numbers = this.typedValue.split('#');
+    let converted = '';
+    for (const num of numbers) {
+      let code = parseInt(num, 10);
+      if (code >= 0 && code <= 26) {
+        code = code + 65; // Convert to ASCII code
+        const letter = String.fromCharCode(code);
+        converted += letter;
+      }
+    }
+    this.convertedValue = converted;
+  }
+}
