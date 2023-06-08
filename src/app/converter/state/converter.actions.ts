070cecf025c5832513e36f0759be3857be7b64a6
Joey Anthony Tuminez <127705319+joey2400@users.noreply.github.com>
2023-06-08 22:35:04 +0800

    Revert "update: converter with ngrxstore"

--- src/app/converter/state/converter.actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const typeNumber = createAction('[Converter] Type Number', props<{ number: string }>());
-export const addSeparator = createAction('[Converter] Add Separator');
-export const removeLastValue = createAction('[Converter] Remove Last Value');
-export const updateConvertedValue = createAction('[Converter] Update Converted Value');
